948efbdca1af6a933d858cf834a7654da9fb3702
Jean-FrancoisVo <jeanfrancoisvo7@gmail.com>
2019-03-02 08:28:09 +0100

    App initialization

---
+++ initializr-app/src/main/kotlin/fr/edf/initializr/Application.kt
@@ -0,0 +1,11 @@
+package fr.edf.initializr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
